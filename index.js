2f9d7d8985c4d1d8275c61e72bad139b097944a9
sviatoslav-sh-halo-lab <sviatoslav.sh@halo-lab.team>
2024-07-11 01:30:05 +0300

    added authorization

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const app = express();
 const userId = 822;
 
 app.get('/orders', async (req, res) => {
-  const data = (await axios.get(`${process.env.SPP_API}/orders?filters%5Buser_id%5D%5B%24in%5D%5B%5D=${userId}`)).data;
+  const data = (await axios.get(`${process.env.SPP_API}/orders?filters%5Buser_id%5D%5B%24in%5D%5B%5D=${userId}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.SPP_SECRET}`,
+    }
+  })).data;
   res.json(data);
 })
 
