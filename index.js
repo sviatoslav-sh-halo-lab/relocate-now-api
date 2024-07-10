9e833d451b40fbe511871e8de49d66d8a24d064a
sviatoslav-sh-halo-lab <sviatoslav.sh@halo-lab.team>
2024-07-11 01:22:17 +0300

    updated code

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import axios from 'axios';
 
 dotenv.config();
 
 const app = express();
 
-const user_id = 822;
+const userId = 822;
 
-app.get('/orders', (req, res) => {
-
+app.get('/orders', async (req, res) => {
+  const data = (await axios.get(`${process.env.SPP_API}/orders?filters%5Buser_id%5D%5B%24in%5D%5B%5D=${userId}`)).data;
+  res.json(data);
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log('Server has started on port ' + process.env.PORT);
 })
